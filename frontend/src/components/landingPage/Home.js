fd569401a6313750dada3c08dc140f80b729f0be
Bentil Okyere Boadu <91896057+Bentil-OkyereBoadu@users.noreply.github.com>
2022-04-04 12:18:34 +0000

    join spotify login

--- frontend/src/components/landingPage/Home.js
+++ frontend/src/components/landingPage/Home.js
@@ -6,6 +6,8 @@ import "./styles.css";
 // const client_id ="ddc7d259bece4112b9df90559ea0e4ff";
 const client_id = "40e0e3786cb34441b74263af7dcb1200";
 const redirect_uri = "http://localhost:3000/session";
+const joinredirect_uri = "http://localhost:3000/join";
+
 
 const OAUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const SCOPES = [
@@ -32,7 +34,8 @@ const Home = () => {
   };
 
   const joinSession = () => {
-    history.push("/join");
+    window.location = `${OAUTH_ENDPOINT}?client_id=${client_id}&redirect_uri=${joinredirect_uri}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+
   };
 
   return (

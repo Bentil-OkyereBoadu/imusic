af31c63a5d656adf33c99f7fc600fb7541782833
Bentil Okyere Boadu <91896057+Bentil-OkyereBoadu@users.noreply.github.com>
2022-04-05 09:56:43 +0000

    sharing

--- frontend/src/components/joinSession/JoinFooter.js
+++ frontend/src/components/joinSession/JoinFooter.js
@@ -1,18 +1,17 @@
-import { Box, Button, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, HStack } from "@chakra-ui/react";
 import React, { useCallback } from "react";
 import { BsShare } from "react-icons/bs";
-import { IoPersonAddOutline } from "react-icons/io5";
 import { SessionState } from "../../context/SessionProvider";
 import { spotifyApi } from "../musicRoom/Playlist";
 import SpotifyPlayer from "react-spotify-web-playback";
+import { FaCopy, FaFacebook, FaTwitter, FaWhatsapp } from "react-icons/fa";
+import { Simplesharer } from "simple-sharer";
 
 const JoinFooter = () => {
   const { token, selectedSession } = SessionState();
   let trackURIs = selectedSession.playlist.map((track) => track.uri);
   spotifyApi.setAccessToken(token);
 
- 
- 
   // Get Information About The User's Current Playback State
   spotifyApi.getMyCurrentPlaybackState().then(
     function(data) {
@@ -32,8 +31,13 @@ const JoinFooter = () => {
     console.group(`RSWP: ${type}`);
     console.log(state);
     console.groupEnd();
-
   }, []);
+
+  const sharing = new Simplesharer();
+  sharing.url = `http://localhost:4000/api/session/${selectedSession._id}/join`;
+  sharing.title = `Join ${selectedSession.name} now and enjoy some good music`;
+  sharing.text = `Join ${selectedSession.name} now and enjoy some good music`;
+
 
   return (
     <Flex
@@ -66,24 +70,37 @@ const JoinFooter = () => {
           />
         </Box>
       )}
-      <Box w="20%" h="100%"></Box>
+      <Box w="10%" h="100%"></Box>
       <Flex
-        w="30%"
+        w="40%"
         justifyContent="space-evenly"
         alignContent="space-between"
         alignItems="center"
         p={1}
       >
-        <Button leftIcon={<BsShare />} colorScheme="blue">
-          Share
-        </Button>
-        <Button
-          rightIcon={<IoPersonAddOutline />}
-          colorScheme="blue"
-          color="white"
-        >
-          Invite friends
-        </Button>
+        <HStack>
+          <Button
+            colorScheme="facebook"
+            leftIcon={<BsShare />}
+            rightIcon={<FaFacebook />}
+            onClick={() => sharing.share("Facebook")}
+          ></Button>
+          <Button
+            colorScheme="twitter"
+            rightIcon={<FaTwitter />}
+            leftIcon={<BsShare />}
+            onClick={() => sharing.twitter()}
+          ></Button>
+          <Button
+            rightIcon={<FaWhatsapp />}
+            leftIcon={<BsShare />}
+            colorScheme="whatsapp"
+            onClick={() => sharing.whatsapp()}
+          ></Button>
+          <Button onClick={() => sharing.copy()} leftIcon={<FaCopy />}>
+            Copy link
+          </Button>
+        </HStack>
         {/* <NavLink to="/">
           <Button colorScheme="red" color="white">
             End Session

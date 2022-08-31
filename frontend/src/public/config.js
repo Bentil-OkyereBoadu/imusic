d0844986cbfdda633360449c78cee113de9cd01a
Bentil-OkyereBoadu <bentil.boadu@amalitech.org>
2022-08-31 09:28:53 +0000

    push to prod

--- frontend/src/public/config.js
+++ frontend/src/public/config.js
@@ -1,8 +1,8 @@
-const backendUrl = process.env.DEV_BACKEND
-const frontendUrl = process.env.DEV_SERVER
+// const backendUrl = process.env.DEV_BACKEND
+// const frontendUrl = process.env.DEV_SERVER
 
-// const frontendUrl = process.env.PRODUCTION_URL
-// const backendUrl = process.env.PROD_BACKEND
+const frontendUrl = process.env.PRODUCTION_URL
+const backendUrl = process.env.PROD_BACKEND
 
 module.exports = {
     api: backendUrl,
